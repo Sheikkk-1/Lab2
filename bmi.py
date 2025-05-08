5370c4b1b13fea769a5fb91e1efaca92d8e92532
Sheik <smssheik20.24@ichat.sp.edu.sg>
2025-05-08 16:18:51 +0800

    BMI calculation and classification

--- bmi.py
+++ bmi.py
@@ -1,9 +1,15 @@
 def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    
     bmi = weight / (height * height)
+    print("BMI = " + str(bmi))
 
     if bmi < 18.5:
-        return bmi, -1  
-    elif bmi <= 25.0:
-        return bmi, 0   
+        print("Under Weight")
+    elif bmi >= 18.5 and bmi <= 25.0:
+        print("Normal Weight")
     else:
-        return bmi, 1  
+        print("Over Weight")
+
+calculate_bmi(weight=57, height=1.73)
